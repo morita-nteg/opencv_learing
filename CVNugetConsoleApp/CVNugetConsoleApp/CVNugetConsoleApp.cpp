8a8f50416874d93d64ea62ecf4f3490bba107bf3
morita-nteg <64726147+morita-nteg@users.noreply.github.com>
2021-04-04 00:33:04 +0900

    An example of Console Application with Nuget

---
+++ CVNugetConsoleApp/CVNugetConsoleApp/CVNugetConsoleApp.cpp
@@ -0,0 +1,25 @@
+﻿// Nuget による OpenCV 4.x の設定例
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+    std::cout << "This is an example." << std::endl;
+    auto input_image = cv::imread("test.png");
+
+    if (!input_image.empty()) {
+        cv::namedWindow("show image", cv::WINDOW_AUTOSIZE | cv::WINDOW_FREERATIO);
+        cv::imshow("show image", input_image);
+        cv::waitKey(0);
+    }
+    else {
+        std::cerr << "test.png : not found" << std::endl;
+    }
+
+    return 0;
+}
+
